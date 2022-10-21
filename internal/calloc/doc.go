76b3c2a93f7f901447e94667ee4b847eb127a522
Anuraag Agrawal <anuraaga@gmail.com>
2022-10-21 10:03:49 +0900

    Finish

--- internal/calloc/doc.go
+++
@@ -1,6 +0,0 @@
-// Copyright The OWASP Coraza contributors
-// SPDX-License-Identifier: Apache-2.0
-
-// Package calloc reimplements some memory allocation functions that C++ expects to exist
-// but TinyGo does not provide.
-package calloc
