a128f613fdf6f7280549221954ee9387bb37d39f
Anuraag Agrawal <anuraaga@gmail.com>
2022-10-24 12:15:12 +0900

    Format

--- internal/gc/doc.go
+++ internal/gc/doc.go
@@ -1,3 +1,6 @@
+// Copyright The OWASP Coraza contributors
+// SPDX-License-Identifier: Apache-2.0
+
 // Package gc is a custom gargabe gollector for TinyGo. The main difference is instead of taking
 // ownership of the entire process heap, it uses malloc to allocate blocks for the GC to then
 // assign to allocated objects.
