5151475ff5385b1d3f78de2a9924943799b596a1
Matteo Pace <pace.matteo96@gmail.com>
2023-01-02 13:14:02 +0100

    adds ENVOY_IMAGE variable for ftw

--- magefiles/magefile.go
+++ magefiles/magefile.go
@@ -237,6 +237,7 @@ func Ftw() error {
 	}()
 	env := map[string]string{
 		"FTW_CLOUDMODE": os.Getenv("FTW_CLOUDMODE"),
+		"ENVOY_IMAGE":   os.Getenv("ENVOY_IMAGE"),
 	}
 	if os.Getenv("ENVOY_NOWASM") == "true" {
 		env["ENVOY_CONFIG"] = "/conf/envoy-config-nowasm.yaml"
