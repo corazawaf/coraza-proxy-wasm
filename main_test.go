9f91b6f2f576faf8955d061d220d0a51dcfb6394
Matteo Pace <pace.matteo96@gmail.com>
2023-02-01 10:42:57 +0100

    new body write for response, added tests for reject and process partial (#136)

--- main_test.go
+++ main_test.go
@@ -55,13 +55,15 @@ func TestLifecycle(t *testing.T) {
 	respBody := []byte(`Hello, yogi!`)
 
 	tests := []struct {
-		name               string
-		inlineRules        string
-		requestHdrsAction  types.Action
-		requestBodyAction  types.Action
-		responseHdrsAction types.Action
-		responded403       bool
-		respondedNullBody  bool
+		name                                string
+		inlineRules                         string
+		requestHdrsAction                   types.Action
+		requestBodyAction                   types.Action
+		responseHdrsAction                  types.Action
+		responded403                        bool
+		responded413                        bool
+		respondedNullBody                   bool
+		expectResponseRejectSinceFirstChunk bool
 	}{
 		{
 			name:               "no rules",
@@ -225,6 +227,28 @@ func TestLifecycle(t *testing.T) {
 			respondedNullBody:  false,
 		},
 		{
+			name: "request body accepted, payload above process partial",
+			inlineRules: `
+		SecRuleEngine On\nSecRequestBodyAccess On\nSecRequestBodyLimit 2\nSecRequestBodyLimitAction ProcessPartial\nSecRule REQUEST_BODY \"animal=bear\" \"id:101,phase:2,t:lowercase,deny\"
+		`,
+			requestHdrsAction:  types.ActionContinue,
+			requestBodyAction:  types.ActionContinue,
+			responseHdrsAction: types.ActionContinue,
+			responded403:       false,
+			respondedNullBody:  false,
+		},
+		{
+			name: "request body denied, above limits",
+			inlineRules: `
+			SecRuleEngine On\nSecRequestBodyAccess On\nSecRequestBodyLimit 2\nSecRule REQUEST_BODY \"name=yogi\" \"id:101,phase:2,t:lowercase,deny\"
+			`,
+			requestHdrsAction:  types.ActionContinue,
+			requestBodyAction:  types.ActionPause,
+			responseHdrsAction: types.ActionContinue,
+			responded413:       true,
+			respondedNullBody:  false,
+		},
+		{
 			name: "status accepted",
 			inlineRules: `
 			SecRuleEngine On\nSecRule RESPONSE_STATUS \"500\" \"id:101,phase:3,t:lowercase,deny\"
@@ -356,6 +380,29 @@ func TestLifecycle(t *testing.T) {
 			responded403:       false,
 			respondedNullBody:  false,
 		},
+		{
+			name: "response body accepted, payload above process partial",
+			inlineRules: `
+			SecRuleEngine On\nSecResponseBodyAccess On\nSecResponseBodyLimit 2\nSecResponseBodyLimitAction ProcessPartial\nSecRule RESPONSE_BODY \"@contains hello\" \"id:101,phase:4,t:lowercase,deny\"
+			`,
+			requestHdrsAction:  types.ActionContinue,
+			requestBodyAction:  types.ActionContinue,
+			responseHdrsAction: types.ActionContinue,
+			responded403:       false,
+			respondedNullBody:  false,
+		},
+		{
+			name: "response body denied, above limits",
+			inlineRules: `
+			SecRuleEngine On\nSecResponseBodyAccess On\nSecResponseBodyLimit 2\nSecResponseBodyLimitAction Reject\nSecRule RESPONSE_BODY \"@contains hello\" \"id:101,phase:4,t:lowercase,deny\"
+			`,
+			requestHdrsAction:                   types.ActionContinue,
+			requestBodyAction:                   types.ActionContinue,
+			responseHdrsAction:                  types.ActionContinue,
+			responded403:                        false, // proxy-wasm does not support it at phase 4
+			respondedNullBody:                   true,
+			expectResponseRejectSinceFirstChunk: true,
+		},
 	}
 
 	vmTest(t, func(t *testing.T, vm types.VMContext) {
@@ -430,7 +477,10 @@ func TestLifecycle(t *testing.T) {
 						}
 						responseBodyAction := host.CallOnResponseBody(id, body, eos)
 						switch {
-						case eos:
+						// expectResponseRejectLimitActionSinceFirstChunk: writing the first chunk (len(respBody) bytes), it is expected to reach
+						// the ResponseBodyLimit with the Action set to Reject. When these conditions happen, ActionContinue will be returned,
+						// with the interruption enforced replacing the body with null bytes (checked with tt.respondedNullBody)
+						case eos, tt.expectResponseRejectSinceFirstChunk:
 							requireEqualAction(t, types.ActionContinue, responseBodyAction, "unexpected response body action, want %q, have %q on end of stream")
 						case responseBodyAccess:
 							requireEqualAction(t, types.ActionPause, responseBodyAction, "unexpected response body action, want %q, have %q")
@@ -444,16 +494,20 @@ func TestLifecycle(t *testing.T) {
 				host.CompleteHttpContext(id)
 
 				pluginResp := host.GetSentLocalResponse(id)
-				if tt.responded403 {
+				switch {
+				case tt.responded403:
 					require.NotNil(t, pluginResp)
 					require.EqualValues(t, 403, pluginResp.StatusCode)
-				} else {
+				case tt.responded413:
+					require.NotNil(t, pluginResp)
+					require.EqualValues(t, 413, pluginResp.StatusCode)
+				default:
 					require.Nil(t, pluginResp)
 				}
 				if tt.respondedNullBody {
 					pluginBodyResp := host.GetCurrentResponseBody(id)
 					require.NotNil(t, pluginBodyResp)
-					require.EqualValues(t, byte('\x00'), pluginBodyResp[0])
+					require.EqualValues(t, bytes.Repeat([]byte("\x00"), len(pluginBodyResp)), pluginBodyResp)
 				}
 			})
 		}
