b37b4ab420a1cd69132a641bc843bbfba88f4890
Anuraag Agrawal <anuraaga@gmail.com>
2023-02-01 14:19:23 +0900

    Remove memstats for now

--- wasmplugin/memstats_on.go
+++ wasmplugin/memstats_on.go
@@ -5,23 +5,6 @@
 
 package wasmplugin
 
-import (
-	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm"
-
-	"github.com/corazawaf/coraza-proxy-wasm/internal/gc"
-)
-
+// TODO(anuraaga): Restore MemStats
 func logMemStats() {
-	ms := gc.MemStats{}
-	gc.ReadMemStats(&ms)
-	proxywasm.LogDebugf(
-		"Sys: %d, HeapSys: %d, HeapAlloc: %d, HeapIdle: %d, HeapReleased: %d, TotalAlloc: %d, NumGC: %d, BytesSinceGC: %d",
-		ms.Sys,
-		ms.HeapSys,
-		ms.HeapAlloc,
-		ms.HeapIdle,
-		ms.HeapReleased,
-		ms.TotalAlloc,
-		ms.NumGC,
-		ms.BytesSinceGC)
 }
