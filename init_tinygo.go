f2732a8cca81c9cafc0e49f5b2c71c24a5a8c8aa
Anuraag Agrawal <anuraaga@gmail.com>
2023-06-22 10:57:12 +0900

    Apply suggestions from code review
    Co-authored-by: Crypt Keeper <64215+codefromthecrypt@users.noreply.github.com>

--- init_tinygo.go
+++ init_tinygo.go
@@ -14,18 +14,22 @@ import (
 // Some host functions that are not implemented by Envoy end up getting imported anyways
 // by code that gets compiled but not executed at runtime. Because we know they are not
 // executed, we can stub them out to allow functioning on Envoy. Note, these match the
-// names and signatures of libc, not WASI ABI.
+// names and signatures of wasi-libc, used by TinyGo, not WASI ABI. Review these exports when either
+// the minimum supported version of Envoy changes or the maximum version of TinyGo.
 
 //export sched_yield
 func sched_yield() int32 {
 	return 0
 }
 
+// fdopendir is re-exported to avoid TinyGo 0.28's import of wasi_snapshot_preview1.fd_readdir.
 //export fdopendir
 func fdopendir(fd int32) unsafe.Pointer {
 	return nil
 }
 
+```suggestion
+// readdir is re-exported to avoid TinyGo 0.28's import of wasi_snapshot_preview1.fd_readdir.
 //export readdir
 func readdir(unsafe.Pointer) unsafe.Pointer {
 	return nil
