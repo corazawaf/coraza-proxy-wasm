5bee881a7e7947a1de7c648d151bf1c93df6bc40
Anuraag Agrawal <anuraaga@gmail.com>
2023-09-15 13:15:54 +0900

    Update TinyGo to 0.29

--- magefiles/magefile.go
+++ magefiles/magefile.go
@@ -20,7 +20,7 @@ import (
 )
 
 var minGoVersion = "1.20"
-var tinygoMinorVersion = "0.28"
+var tinygoMinorVersion = "0.29"
 var addLicenseVersion = "04bfe4ee9ca5764577b029acc6a1957fd1997153" // https://github.com/google/addlicense
 var golangCILintVer = "v1.54.2"                                    // https://github.com/golangci/golangci-lint/releases
 var gosImportsVer = "v0.3.1"                                       // https://github.com/rinchsan/gosimports/releases/tag/v0.3.1
